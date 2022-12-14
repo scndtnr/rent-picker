3cf60ed6fa2b8f5516fe30a84529b44fd0d65706
scndtnr <scndtnr@users.noreply.github.com>
2022-12-14 15:28:03 +0900

    add get_usize_of_env_var for shorthand.

--- web-scraping/usecase/src/env.rs
+++ web-scraping/usecase/src/env.rs
@@ -2,6 +2,13 @@ pub fn get_env_var(name: &str) -> Result<String, String> {
     std::env::var(name).map_err(|e| format!("{}: {}", name, e))
 }
 
+pub fn get_usize_of_env_var(name: &str) -> usize {
+    get_env_var(name)
+        .unwrap()
+        .parse()
+        .unwrap_or_else(|_| panic!("Fail to parse {} into usize.", name))
+}
+
 pub fn get_bool_of_env_var(name: &str) -> bool {
     get_env_var(name).unwrap().to_uppercase() == "TRUE"
 }
