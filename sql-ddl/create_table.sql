92e792be2ecd988b28640a894305f875ea6b5835
scndtnr <scndtnr@users.noreply.github.com>
2022-12-20 16:05:24 +0900

    add room_raw table definition

--- sql-ddl/create_table.sql
+++ sql-ddl/create_table.sql
@@ -1,5 +1,7 @@
 -- room_header definition
 
+/* --------- room_header ------------- */
+
 -- DROP TABLE room_header;
 CREATE TABLE room_header (
     -- 詳細ページのURL
@@ -43,64 +45,56 @@ CREATE TABLE room_header (
 
 -- DROP TABLE load_room_header;
 CREATE TABLE load_room_header (
-    -- 詳細ページのURL
     url VARCHAR NOT NULL,
-    -- 建物名
     building_name VARCHAR,
-    -- 所在地
     location VARCHAR,
-    -- 駅徒歩
     walk_to_station VARCHAR,
-    -- 築年数
     age_in_years VARCHAR,
-    -- 階建
     number_of_floors VARCHAR,
-    -- 乗換回数（検索結果）
     transfer_in_search_result VARCHAR,
-    -- 都道府県エリア（検索条件）
     area_of_search_condition VARCHAR,
-    -- 通勤先の最寄り駅（検索条件）
     commute_station_of_search_condition VARCHAR,
-    -- 階
     floor VARCHAR,
-    -- 家賃
     rental_fee VARCHAR,
-    -- 管理費共益費
     management_fee VARCHAR,
-    -- 敷金
     security_deposit VARCHAR,
-    -- 礼金
     key_money VARCHAR,
-    -- 間取り
     floor_plan VARCHAR,
-    -- 専有面積
     private_area VARCHAR,
-    -- スクレイピングした日時
     scraping_date  DATETIME
 );
 
 -- DROP TABLE temp_room_header;
 CREATE TABLE temp_room_header (
+    url VARCHAR NOT NULL,
+    building_name VARCHAR,
+    location VARCHAR,
+    walk_to_station VARCHAR,
+    age_in_years VARCHAR,
+    number_of_floors VARCHAR,
+    transfer_in_search_result VARCHAR,
+    area_of_search_condition VARCHAR,
+    commute_station_of_search_condition VARCHAR,
+    floor VARCHAR,
+    rental_fee VARCHAR,
+    management_fee VARCHAR,
+    security_deposit VARCHAR,
+    key_money VARCHAR,
+    floor_plan VARCHAR,
+    private_area VARCHAR,
+    scraping_date  DATETIME
+);
+
+/* --------- room_raw ------------- */
+
+-- DROP TABLE room_raw;
+CREATE TABLE room_raw (
     -- 詳細ページのURL
     url VARCHAR NOT NULL,
+    -- suumo物件コード
+    suumo_code VARCHAR,
     -- 建物名
     building_name VARCHAR,
-    -- 所在地
-    location VARCHAR,
-    -- 駅徒歩
-    walk_to_station VARCHAR,
-    -- 築年数
-    age_in_years VARCHAR,
-    -- 階建
-    number_of_floors VARCHAR,
-    -- 乗換回数（検索結果）
-    transfer_in_search_result VARCHAR,
-    -- 都道府県エリア（検索条件）
-    area_of_search_condition VARCHAR,
-    -- 通勤先の最寄り駅（検索条件）
-    commute_station_of_search_condition VARCHAR,
-    -- 階
-    floor VARCHAR,
     -- 家賃
     rental_fee VARCHAR,
     -- 管理費共益費
@@ -109,14 +103,138 @@ CREATE TABLE temp_room_header (
     security_deposit VARCHAR,
     -- 礼金
     key_money VARCHAR,
+    -- 保証金
+    guarantee_deposit VARCHAR,
+    -- 敷引償却
+    key_money_amortization VARCHAR,
+    -- 所在地
+    location VARCHAR,
+    -- 駅徒歩
+    walk_to_station VARCHAR,
     -- 間取り
     floor_plan VARCHAR,
+    -- 間取り詳細
+    floor_plan_details VARCHAR,
     -- 専有面積
     private_area VARCHAR,
+    -- 築年数
+    age_in_years VARCHAR,
+    -- 築年月
+    age_in_months VARCHAR,
+    -- 階
+    floor VARCHAR,
+    -- 階建
+    number_of_floors VARCHAR,
+    -- 向き
+    facing_direction VARCHAR,
+    -- 建物種別
+    building_type VARCHAR,
+    -- 部屋の特徴設備
+    features VARCHAR,
+    -- 構造
+    structure VARCHAR,
+    -- 損保
+    damage_insurance VARCHAR,
+    -- 駐車場
+    parking VARCHAR,
+    -- 入居（時期）
+    move_in VARCHAR,
+    -- 取引態様
+    transaction_type VARCHAR,
+    -- 条件
+    conditions VARCHAR,
+    -- 取り扱い店舗物件コード
+    property_code VARCHAR,
+    -- 情報更新日
+    info_update_date VARCHAR,
+    -- 次回更新日
+    next_update_date VARCHAR,
+    -- 契約期間
+    contract_period VARCHAR,
+    -- 備考
+    notes VARCHAR,
     -- スクレイピングした日時
-    scraping_date  DATETIME
+    scraping_date DATETIME,
+    ---------------------------------
+    -- PK制約
+    PRIMARY KEY (url)
 );
 
--- litestream に自動生成されるテーブル
+-- DROP TABLE load_room_raw;
+CREATE TABLE load_room_raw (
+    url VARCHAR NOT NULL,
+    suumo_code VARCHAR,
+    building_name VARCHAR,
+    rental_fee VARCHAR,
+    management_fee VARCHAR,
+    security_deposit VARCHAR,
+    key_money VARCHAR,
+    guarantee_deposit VARCHAR,
+    key_money_amortization VARCHAR,
+    location VARCHAR,
+    walk_to_station VARCHAR,
+    floor_plan VARCHAR,
+    floor_plan_details VARCHAR,
+    private_area VARCHAR,
+    age_in_years VARCHAR,
+    age_in_months VARCHAR,
+    floor VARCHAR,
+    number_of_floors VARCHAR,
+    facing_direction VARCHAR,
+    building_type VARCHAR,
+    features VARCHAR,
+    structure VARCHAR,
+    damage_insurance VARCHAR,
+    parking VARCHAR,
+    move_in VARCHAR,
+    transaction_type VARCHAR,
+    conditions VARCHAR,
+    property_code VARCHAR,
+    info_update_date VARCHAR,
+    next_update_date VARCHAR,
+    contract_period VARCHAR,
+    notes VARCHAR,
+    scraping_date DATETIME
+);
+
+-- DROP TABLE temp_room_raw;
+CREATE TABLE temp_room_raw (
+    url VARCHAR NOT NULL,
+    suumo_code VARCHAR,
+    building_name VARCHAR,
+    rental_fee VARCHAR,
+    management_fee VARCHAR,
+    security_deposit VARCHAR,
+    key_money VARCHAR,
+    guarantee_deposit VARCHAR,
+    key_money_amortization VARCHAR,
+    location VARCHAR,
+    walk_to_station VARCHAR,
+    floor_plan VARCHAR,
+    floor_plan_details VARCHAR,
+    private_area VARCHAR,
+    age_in_years VARCHAR,
+    age_in_months VARCHAR,
+    floor VARCHAR,
+    number_of_floors VARCHAR,
+    facing_direction VARCHAR,
+    building_type VARCHAR,
+    features VARCHAR,
+    structure VARCHAR,
+    damage_insurance VARCHAR,
+    parking VARCHAR,
+    move_in VARCHAR,
+    transaction_type VARCHAR,
+    conditions VARCHAR,
+    property_code VARCHAR,
+    info_update_date VARCHAR,
+    next_update_date VARCHAR,
+    contract_period VARCHAR,
+    notes VARCHAR,
+    scraping_date DATETIME
+);
+
+/* -- litestream に自動生成されるテーブル -- */
+
 CREATE TABLE _litestream_seq (id INTEGER PRIMARY KEY, seq INTEGER);
 CREATE TABLE _litestream_lock (id INTEGER);
--- web-scraping/domain/src/model/room_raw.rs
+++ web-scraping/domain/src/model/room_raw.rs
@@ -1,37 +1,175 @@
+use chrono::{DateTime, FixedOffset};
 use derive_new::new;
 
 #[allow(clippy::too_many_arguments)]
 #[derive(new, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct RoomRaw {
-    建物名: String,
-    家賃: String,
-    管理費共益費: String,
-    敷金: String,
-    礼金: String,
-    保証金: String,
-    敷引償却: String,
-    所在地: String,
-    駅徒歩: String,
-    間取り: String,
-    専有面積: String,
-    築年数: String,
-    階: String,
-    向き: String,
-    建物種別: String,
-    部屋の特徴設備: String,
-    間取り詳細: String,
-    構造: String,
-    階建: String,
-    築年月: String,
-    損保: String,
-    駐車場: String,
-    入居: String,
-    取引態様: String,
-    条件: String,
-    取り扱い店舗物件コード: String,
-    suumo物件コード: String,
-    情報更新日: String,
-    次回更新日: String,
-    契約期間: String,
-    備考: String,
+    /// 詳細ページのURL
+    url: String,
+    /// suumo物件コード
+    suumo_code: String,
+    /// 建物名
+    building_name: String,
+    /// 家賃
+    rental_fee: String,
+    /// 管理費共益費
+    management_fee: String,
+    /// 敷金
+    security_deposit: String,
+    /// 礼金
+    key_money: String,
+    /// 保証金
+    guarantee_deposit: String,
+    /// 敷引償却
+    key_money_amortization: String,
+    /// 所在地
+    location: String,
+    /// 駅徒歩
+    walk_to_station: String,
+    /// 間取り
+    floor_plan: String,
+    /// 間取り詳細
+    floor_plan_details: String,
+    /// 専有面積
+    private_area: String,
+    /// 築年数
+    age_in_years: String,
+    /// 築年月
+    age_in_months: String,
+    /// 階
+    floor: String,
+    /// 階建
+    number_of_floors: String,
+    /// 向き
+    facing_direction: String,
+    /// 建物種別
+    building_type: String,
+    /// 部屋の特徴設備
+    features: String,
+    /// 構造
+    structure: String,
+    /// 損保
+    damage_insurance: String,
+    /// 駐車場
+    parking: String,
+    /// 入居（時期）
+    move_in: String,
+    /// 取引態様
+    transaction_type: String,
+    /// 条件
+    conditions: String,
+    /// 取り扱い店舗物件コード
+    property_code: String,
+    /// 情報更新日
+    info_update_date: String,
+    /// 次回更新日
+    next_update_date: String,
+    /// 契約期間
+    contract_period: String,
+    /// 備考
+    notes: String,
+    /// スクレイピングした日時
+    scraping_date: DateTime<FixedOffset>,
+}
+
+impl RoomRaw {
+    pub fn url(&self) -> &str {
+        &self.url
+    }
+    pub fn suumo_code(&self) -> &str {
+        &self.suumo_code
+    }
+    pub fn building_name(&self) -> &str {
+        &self.building_name
+    }
+    pub fn rental_fee(&self) -> &str {
+        &self.rental_fee
+    }
+    pub fn management_fee(&self) -> &str {
+        &self.management_fee
+    }
+    pub fn security_deposit(&self) -> &str {
+        &self.security_deposit
+    }
+    pub fn key_money(&self) -> &str {
+        &self.key_money
+    }
+    pub fn guarantee_deposit(&self) -> &str {
+        &self.guarantee_deposit
+    }
+    pub fn key_money_amortization(&self) -> &str {
+        &self.key_money_amortization
+    }
+    pub fn location(&self) -> &str {
+        &self.location
+    }
+    pub fn walk_to_station(&self) -> &str {
+        &self.walk_to_station
+    }
+    pub fn floor_plan(&self) -> &str {
+        &self.floor_plan
+    }
+    pub fn floor_plan_details(&self) -> &str {
+        &self.floor_plan_details
+    }
+    pub fn private_area(&self) -> &str {
+        &self.private_area
+    }
+    pub fn age_in_years(&self) -> &str {
+        &self.age_in_years
+    }
+    pub fn age_in_months(&self) -> &str {
+        &self.age_in_months
+    }
+    pub fn floor(&self) -> &str {
+        &self.floor
+    }
+    pub fn number_of_floors(&self) -> &str {
+        &self.number_of_floors
+    }
+    pub fn facing_direction(&self) -> &str {
+        &self.facing_direction
+    }
+    pub fn building_type(&self) -> &str {
+        &self.building_type
+    }
+    pub fn features(&self) -> &str {
+        &self.features
+    }
+    pub fn structure(&self) -> &str {
+        &self.structure
+    }
+    pub fn damage_insurance(&self) -> &str {
+        &self.damage_insurance
+    }
+    pub fn parking(&self) -> &str {
+        &self.parking
+    }
+    pub fn move_in(&self) -> &str {
+        &self.move_in
+    }
+    pub fn transaction_type(&self) -> &str {
+        &self.transaction_type
+    }
+    pub fn conditions(&self) -> &str {
+        &self.conditions
+    }
+    pub fn property_code(&self) -> &str {
+        &self.property_code
+    }
+    pub fn info_update_date(&self) -> &str {
+        &self.info_update_date
+    }
+    pub fn next_update_date(&self) -> &str {
+        &self.next_update_date
+    }
+    pub fn contract_period(&self) -> &str {
+        &self.contract_period
+    }
+    pub fn notes(&self) -> &str {
+        &self.notes
+    }
+    pub fn scraping_date(&self) -> DateTime<FixedOffset> {
+        self.scraping_date
+    }
 }
