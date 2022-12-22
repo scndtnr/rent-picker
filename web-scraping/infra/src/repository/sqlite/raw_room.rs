feb6a303f63cc77327974e793389d4301019eca1
scndtnr <scndtnr@users.noreply.github.com>
2022-12-22 17:13:33 +0900

    impl RawRoomRepository.

--- web-scraping/infra/src/repository/sqlite/raw_room.rs
+++ web-scraping/infra/src/repository/sqlite/raw_room.rs
@@ -1,9 +1,15 @@
-use super::SqliteRepositoryImpl;
+use crate::{
+    model::RawRoomRecord,
+    progress_bar::{debug_progress, new_progress_bar},
+};
+
+use super::{sql, SqliteRepositoryImpl};
 use anyhow::Result;
 use domain::{
     model::{AsVec, RawRoom, RawRooms, TableType},
     repository::RawRoomRepository,
 };
+use sqlx::{QueryBuilder, Sqlite};
 
 #[async_trait::async_trait]
 impl RawRoomRepository for SqliteRepositoryImpl<RawRoom> {
@@ -20,33 +26,119 @@ impl RawRoomRepository for SqliteRepositoryImpl<RawRoom> {
 
     /// 複数のデータを500件ずつinsertする。
     #[tracing::instrument(level = "debug", skip_all, fields(len=source.len(),table=table.to_string()), err(Debug))]
-    #[allow(unused_variables)]
     async fn insert_many_multi(&self, source: &RawRooms, table: TableType) -> Result<()> {
-        todo!();
+        // プログレスバーの準備
+        let target_table = sql::raw_room::table_name(&table);
+        let pb_records = new_progress_bar(source.len() as u64).await;
+        pb_records.set_message(format!("Insert to {}...", target_table));
+
+        // RawRoomsをDTOのVecに変換する
+        let dto_raw_rooms: Vec<RawRoomRecord> = source
+            .clone()
+            .into_inner()
+            .into_iter()
+            .map(|raw_room| raw_room.into())
+            .collect();
+
+        // n件毎にinsertするクエリビルダを生成する
+        let n = 500;
+        let query_builders: Vec<QueryBuilder<Sqlite>> = dto_raw_rooms
+            .chunks(n)
+            .map(|raw_rooms| {
+                let mut query_builder: QueryBuilder<Sqlite> =
+                    QueryBuilder::new(sql::raw_room::insert_all_columns(&table));
+                query_builder.push_values(raw_rooms, |mut b, raw_room| {
+                    b.push_bind(raw_room.url.clone())
+                        .push_bind(raw_room.suumo_code.clone())
+                        .push_bind(raw_room.building_name.clone())
+                        .push_bind(raw_room.rental_fee.clone())
+                        .push_bind(raw_room.management_fee.clone())
+                        .push_bind(raw_room.security_deposit.clone())
+                        .push_bind(raw_room.key_money.clone())
+                        .push_bind(raw_room.guarantee_deposit.clone())
+                        .push_bind(raw_room.key_money_amortization.clone())
+                        .push_bind(raw_room.location.clone())
+                        .push_bind(raw_room.walk_to_station.clone())
+                        .push_bind(raw_room.floor_plan.clone())
+                        .push_bind(raw_room.floor_plan_details.clone())
+                        .push_bind(raw_room.private_area.clone())
+                        .push_bind(raw_room.age_in_years.clone())
+                        .push_bind(raw_room.age_in_months.clone())
+                        .push_bind(raw_room.floor.clone())
+                        .push_bind(raw_room.number_of_floors.clone())
+                        .push_bind(raw_room.facing_direction.clone())
+                        .push_bind(raw_room.building_type.clone())
+                        .push_bind(raw_room.features.clone())
+                        .push_bind(raw_room.structure.clone())
+                        .push_bind(raw_room.damage_insurance.clone())
+                        .push_bind(raw_room.parking.clone())
+                        .push_bind(raw_room.move_in.clone())
+                        .push_bind(raw_room.transaction_type.clone())
+                        .push_bind(raw_room.conditions.clone())
+                        .push_bind(raw_room.property_code.clone())
+                        .push_bind(raw_room.contract_period.clone())
+                        .push_bind(raw_room.notes.clone())
+                        .push_bind(raw_room.info_update_date)
+                        .push_bind(raw_room.next_update_date)
+                        .push_bind(raw_room.scraping_date);
+                });
+                query_builder
+            })
+            .collect();
+
+        // n 件毎に insert を実行する
+        let pool = self.writer_pool();
+        for mut builder in query_builders {
+            let query = builder.build();
+            query.execute(&*pool).await?;
+            pb_records.inc(n as u64);
+            debug_progress(&pb_records, "Insert record...").await;
+        }
+
+        // プログレスバーの後始末
+        pb_records.finish_with_message(format!("Finish Insert to {}", target_table));
+
+        // 返り値
+        Ok(())
     }
 
     /// tempテーブルからloadテーブルに全件insertする
     #[tracing::instrument(level = "debug", skip_all, err(Debug))]
     async fn insert_to_load_from_temp_all(&self) -> Result<()> {
-        todo!();
+        let pool = self.writer_pool();
+        let sql = sql::raw_room::insert_from_other_table_all(&TableType::Load, &TableType::Temp);
+        let _ = sqlx::query(&sql).execute(&*pool).await?;
+        Ok(())
     }
 
     /// tempテーブルのPK集約レコードをmainテーブルにinsertする
     #[tracing::instrument(level = "debug", skip_all, err(Debug))]
     async fn insert_to_main_from_temp_group_by_pk(&self) -> Result<()> {
-        todo!();
+        let pool = self.writer_pool();
+        let sql =
+            sql::raw_room::insert_from_other_table_group_by_pk(&TableType::Main, &TableType::Temp);
+        let _ = sqlx::query(&sql).execute(&*pool).await?;
+        Ok(())
     }
 
     /// 対象テーブルのレコードを全件deleteする
     #[tracing::instrument(level = "debug", skip_all, fields(table=table.to_string()), err(Debug))]
-    #[allow(unused_variables)]
     async fn delete_all(&self, table: TableType) -> Result<()> {
-        todo!();
+        let pool = self.writer_pool();
+        let sql = sql::raw_room::delete_all(&table);
+        let _ = sqlx::query(&sql).execute(&*pool).await?;
+        Ok(())
     }
 
     /// tempテーブルのPK集約レコードと合致するレコードをmainテーブルから削除する
     #[tracing::instrument(level = "debug", skip_all, err(Debug))]
     async fn delete_from_main_by_temp_record_pk(&self) -> Result<()> {
-        todo!();
+        let pool = self.writer_pool();
+        let sql = sql::raw_room::delete_where_group_by_pk_from_other_table(
+            &TableType::Main,
+            &TableType::Temp,
+        );
+        let _ = sqlx::query(&sql).execute(&*pool).await?;
+        Ok(())
     }
 }
--- web-scraping/infra/src/repository/sqlite/sql/raw_room.rs
+++ web-scraping/infra/src/repository/sqlite/sql/raw_room.rs
@@ -24,3 +24,164 @@ pub fn select_updated_url(table: &TableType) -> String {
         table
     )
 }
+
+/// PK毎にスクレイピング日時が最大のレコードを集約する select 文
+pub fn select_group_by_pk(table: &TableType) -> String {
+    let table = self::table_name(table);
+    format!(
+        "
+        SELECT
+            t.url
+            ,t.suumo_code
+            ,t.building_name
+            ,t.rental_fee
+            ,t.management_fee
+            ,t.security_deposit
+            ,t.key_money
+            ,t.guarantee_deposit
+            ,t.key_money_amortization
+            ,t.location
+            ,t.walk_to_station
+            ,t.floor_plan
+            ,t.floor_plan_details
+            ,t.private_area
+            ,t.age_in_years
+            ,t.age_in_months
+            ,t.floor
+            ,t.number_of_floors
+            ,t.facing_direction
+            ,t.building_type
+            ,t.features
+            ,t.structure
+            ,t.damage_insurance
+            ,t.parking
+            ,t.move_in
+            ,t.transaction_type
+            ,t.conditions
+            ,t.property_code
+            ,t.contract_period
+            ,t.notes
+            ,t.info_update_date
+            ,t.next_update_date
+            ,t.scraping_date
+            FROM
+                {} t
+                JOIN (
+                    SELECT
+                        url,
+                        max(scraping_date) max_scraping_date
+                    FROM
+                        {}
+                    GROUP BY
+                        url
+                ) g
+                    ON t.url = g.url
+                    AND t.scraping_date = g.max_scraping_date
+        ",
+        table, table
+    )
+}
+
+/// raw_room 系テーブルへの insert文
+pub fn insert_all_columns(table: &TableType) -> String {
+    let table = self::table_name(table);
+    format!(
+        "
+        INSERT INTO {}
+            (
+                url
+                ,suumo_code
+                ,building_name
+                ,rental_fee
+                ,management_fee
+                ,security_deposit
+                ,key_money
+                ,guarantee_deposit
+                ,key_money_amortization
+                ,location
+                ,walk_to_station
+                ,floor_plan
+                ,floor_plan_details
+                ,private_area
+                ,age_in_years
+                ,age_in_months
+                ,floor
+                ,number_of_floors
+                ,facing_direction
+                ,building_type
+                ,features
+                ,structure
+                ,damage_insurance
+                ,parking
+                ,move_in
+                ,transaction_type
+                ,conditions
+                ,property_code
+                ,contract_period
+                ,notes
+                ,info_update_date
+                ,next_update_date
+                ,scraping_date
+            )
+        ",
+        table
+    )
+}
+
+/// raw_room 系のテーブルからテーブルへの全件 insert文
+pub fn insert_from_other_table_all(table: &TableType, other: &TableType) -> String {
+    let table = self::table_name(table);
+    let other = self::table_name(other);
+    format!(
+        "
+                INSERT INTO {}
+                SELECT * FROM {}
+            ",
+        table, other
+    )
+}
+
+/// raw_room 系のテーブルからPKで集約したデータを
+/// 同じく raw_room 系のテーブルへ入れ込む insert文
+pub fn insert_from_other_table_group_by_pk(table: &TableType, other: &TableType) -> String {
+    let group_by_pk_from_other = self::select_group_by_pk(other);
+    let table = self::table_name(table);
+    format!(
+        "
+                INSERT INTO {}
+                SELECT other.* FROM ({}) other
+            ",
+        table, group_by_pk_from_other
+    )
+}
+
+/// raw_room 系テーブルのデータを全削除する delete文
+pub fn delete_all(table: &TableType) -> String {
+    let table = self::table_name(table);
+    format!(
+        "
+            DELETE FROM {}
+            ",
+        table
+    )
+}
+
+/// raw_room 系テーブルからPKに合致したレコードを削除する delete文
+/// PKは他の raw_room 系テーブルから引っ張ってくる
+pub fn delete_where_group_by_pk_from_other_table(table: &TableType, other: &TableType) -> String {
+    let table = self::table_name(table);
+    let group_by_pk_from_other = self::select_group_by_pk(other);
+    format!(
+        "
+                DELETE FROM {}
+                WHERE
+                    url in (
+                        SELECT
+                            other.url
+                        FROM
+                            ({}) other
+                    )
+                ",
+        table, group_by_pk_from_other
+    )
+}
